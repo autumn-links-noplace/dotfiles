c86040efb1a3f25b1ca5d444b975d8aa83e4a6e2
Andrew Hartford <foflannail@gmail.com>
2025-02-25 16:53:32 -0500

    Add neovim config separate from vim
    Add neovim config separate from vim. Using lazy.vim as package manger.
    Install some plusings: fzf-lua and neo-tree. Don't have keybindings
    etc setup yet.
    
    Raw setup notes below.
    
    lazy.vim:
    * https://github.com/folke/lazy.nvim?tab=readme-ov-file
    * https://www.reddit.com/r/neovim/comments/15b1gco/what_plugin_manager_are_you_currently_using/
    
    brew install luarocks
    brew install font-jetbrains-mono-nerd-font
    brew install fd
    brew install bat
    brew install git-delta
    
    Create iterm2 profile to use jet brains font. Use that for Neovim.

---
+++ configs/neovim/dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,9 @@
+-- https://github.com/ibhagwan/fzf-lua?tab=readme-ov-file#usage
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {}
+}
