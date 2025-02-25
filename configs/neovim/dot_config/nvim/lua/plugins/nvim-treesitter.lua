2ac9cceffd5ca6d8a6c4f1feb1d5447eb3353f67
Andrew Hartford <foflannail@gmail.com>
2025-02-25 17:23:43 -0500

    Add treesitter neovim plugin
    Add treesitter neovim plugin for syntax highlighting

---
+++ configs/neovim/dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,30 @@
+ return {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { 
+		  "c",
+		  "css", 
+		  "go", 
+		  "html", 
+		  "java",
+		  "javascript", 
+		  "lua", 
+		  "python",
+		  "ruby",
+		  "rust", 
+		  "typescript",
+		  "vim", 
+		  "vimdoc", 
+	  },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+}
