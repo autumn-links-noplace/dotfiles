cd555660c584f9119754198b2eeb1a5eb2124856
Andrew Hartford <andrew.hartford@compass.com>
2025-04-13 10:56:00 -0400

    Compass things and fix history search
    Add Compass things and fix history search by removing syntax
    highlighting plugin.

---
+++ configs/zsh/dot_zsh.d/after/compass.zsh
@@ -0,0 +1,6 @@
+alias cx="compass"
+alias cw="compass workspace"
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+export IGNORE_PYTHON_VERSION_REQUIREMENT="1"  ##compass5ea843
+export GITROOT="/Users/andrew.hartford/development"  ##compass5ea843
