491a7e922041055f1c687e8c6ff2e9627592d919
Andrew Hartford <andrew.hartford@compass.com>
2025-12-22 14:29:28 -0500

    get neotree bindings working

--- configs/neovim/dot_config/nvim/lua/plugins/which-key.lua
+++ configs/neovim/dot_config/nvim/lua/plugins/which-key.lua
@@ -4,15 +4,15 @@ return {
   opts = {
     spec = {
       -- Neotree group
-      { "<M-R>", group = "neotree", mode = "n" },
-      { "<M-R><M-F>", "<cmd>Neotree toggle<cr>", desc = "Toggle File Tree", mode = "n" },
-      { "<M-R><M-R>", "<cmd>Neotree reveal<cr>", desc = "Reveal Current File", mode = "n" },
-      { "<M-R><M-B>", "<cmd>Neotree toggle show buffers right<cr>", desc = "Toggle Buffer Tree", mode = "n" },
+      { "<C-K>", group = "neotree", mode = "n" },
+      { "<C-K>f", "<cmd>Neotree toggle<cr>", desc = "Toggle File Tree", mode = "n" },
+      { "<C-K>r", "<cmd>Neotree reveal<cr>", desc = "Reveal Current File", mode = "n" },
+      { "<C-K>b", "<cmd>Neotree toggle show buffers right<cr>", desc = "Toggle Buffer Tree", mode = "n" },
 
       -- FzfLua group
       { "<C-P>", group = "fuzzy find", mode = "n" },
-      { "<C-P><C-P>", "<cmd>FzfLua files<cr>", desc = "Find Files", mode = "n" },
-      { "<C-P><C-U>", "<cmd>FzfLua buffers<cr>", desc = "Find Buffers", mode = "n" },
+      { "<C-P>p", "<cmd>FzfLua files<cr>", desc = "Find Files", mode = "n" },
+      { "<C-P>u", "<cmd>FzfLua buffers<cr>", desc = "Find Buffers", mode = "n" },
     },
   },
   keys = {
