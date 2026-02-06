24fe6889646b8096d61dfd8f67d363911e406162
Andrew Hartford <andrew.hartford@compass.com>
2026-02-06 13:02:21 -0500

    nvim clipboard tweak

--- configs/neovim/dot_config/nvim/lua/config/clipboard.lua
+++ configs/neovim/dot_config/nvim/lua/config/clipboard.lua
@@ -3,9 +3,9 @@ local M = {}
 function M.setup()
   -- When running inside tmux or over SSH, prefer OSC52 so clipboard
   -- works with the local machine's clipboard via the terminal.
+  -- System clipboard updates when explicitly using + (or * maybe) registers
   if vim.env.SSH_TTY or vim.env.TMUX then
     vim.g.clipboard = "osc52"
-    vim.opt.clipboard:append("unnamedplus")
   end
 end
 
